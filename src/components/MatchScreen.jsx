956af22e628bf2fa2021bc4df83a83410b733220
erickserra <erickv.serra@gmail.com>
2023-05-01 21:49:33 -0300

    refresh bg colors

--- src/components/MatchScreen.jsx
+++ src/components/MatchScreen.jsx
@@ -1,63 +1,61 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { ScreenComponent } from "../constants";
-import { useApp } from "../contexts/AppContext"
+import { useApp } from "../contexts/AppContext";
 import { ScreenOne } from "./screen-one";
 import { ScreenTwo } from "./screen-two";
 import { ScreenThree } from "./screen-three";
 import { ScreenFour } from "./screen-four";
 import { ScreenFive } from "./screen-five";
 import { ScreenSix } from "./screen-six";
-import { useIdleTimer } from 'react-idle-timer'
-
+import { useIdleTimer } from "react-idle-timer";
 
 export function MatchScreen() {
-  const { currentScreen, updateScreen } = useApp()
+  const { currentScreen, updateScreen } = useApp();
+
+  const bg =
+    currentScreen === ScreenComponent.SCREEN_ONE
+      ? "#641964"
+      : currentScreen === ScreenComponent.SCREEN_TWO
+      ? "#641964"
+      : currentScreen === ScreenComponent.SCREEN_THREE
+      ? "#997299"
+      : currentScreen === ScreenComponent.SCREEN_FOUR
+      ? "#f3e191"
+      : currentScreen === ScreenComponent.SCREEN_FIVE
+      ? "#d3c6d4"
+      : "#f3e191";
 
   const onIdle = () => {
-    updateScreen(ScreenComponent.SCREEN_ONE)
-  }
+    updateScreen(ScreenComponent.SCREEN_ONE);
+  };
 
   useIdleTimer({
     onIdle,
     timeout: 1000 * 60 * 1,
-    throttle: 500
-  })
+    throttle: 500,
+  });
 
   function GetCurrentScreen() {
     switch (currentScreen) {
       case ScreenComponent.SCREEN_ONE:
-        return (
-          <ScreenOne />
-        )
+        return <ScreenOne />;
       case ScreenComponent.SCREEN_TWO:
-        return (
-          <ScreenTwo />
-        )
+        return <ScreenTwo />;
       case ScreenComponent.SCREEN_THREE:
-        return (
-          <ScreenThree />
-        )
+        return <ScreenThree />;
       case ScreenComponent.SCREEN_FOUR:
-        return (
-          <ScreenFour />
-        )
+        return <ScreenFour />;
       case ScreenComponent.SCREEN_FIVE:
-        return (
-          <ScreenFive />
-        )
+        return <ScreenFive />;
       case ScreenComponent.SCREEN_SIX:
-        return (
-          <ScreenSix />
-        )
+        return <ScreenSix />;
       default:
-        return (
-          <ScreenOne />
-        )
+        return <ScreenOne />;
     }
   }
 
   return (
-    <div style={{ height: '100%', backgroundColor: '#f2e9c1' }}>
+    <div style={{ height: "100%", backgroundColor: bg }}>
       <AnimatePresence mode="wait">
         <motion.div
           key={currentScreen}
@@ -65,11 +63,11 @@ export function MatchScreen() {
           animate={{ x: 0, opacity: 1 }}
           exit={{ x: -100, opacity: 0 }}
           transition={{ duration: 0.2 }}
-          style={{ height: '100%' }}
+          style={{ height: "100%" }}
         >
           {GetCurrentScreen()}
         </motion.div>
       </AnimatePresence>
     </div>
-  )
+  );
 }
